23f0cb14fdb15cbda4ecc399fcc3443dec2d5900
Carlos <cgomessilva2015@gmail.com>
2023-01-18 16:32:05 -0300

    aula variaveis

---
+++ variaveis/script -aula.js
@@ -0,0 +1,29 @@
+var nome = "Carlos";
+var idade = 25;
+var possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Antonio",
+  cidade = "Jandira - SP";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+//console.log(aindaNaoDefine);
+
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+/* const time = "Flamengo";
+time = "Vasco";
+console.log(time); */
---
+++ variaveis/script.js
@@ -0,0 +1,17 @@
+// Declarar uma variável com o seu nome
+var nome = "Carlos";
+
+// Declarar uma variável com a sua idade
+var idade = 30;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+
+console.log(nome, idade, comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+var nome, idade, formacao, time, pais;
