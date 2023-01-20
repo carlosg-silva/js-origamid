6c44cd4542f5f5001d42b60faf47fa877eb9472e
Carlos <cgomessilva2015@gmail.com>
2023-01-20 16:30:00 -0300

    funcoes aula 1

---
+++ javascript-para-iniciantes/funcoes/script.js
@@ -0,0 +1,33 @@
+function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+console.log(areaQuadrado(10));
+
+function pi() {
+  return 3.14;
+}
+
+var total = 5 * pi();
+console.log(total);
+
+function imc(peso, altura) {
+  var imc = peso / (altura * altura);
+  return imc;
+}
+console.log(imc(80, 1.8));
+
+function corFavorita(cor) {
+  if (cor === "azul") {
+    return "Eu gosto do céu";
+  } else if (cor === "verde") {
+    return "Eu gosto da grama";
+  } else {
+    return "Eu não gosto de cores";
+  }
+}
+console.log(corFavorita());
+
+addEventListener("click", function () {
+  console.log("Oi");
+});
