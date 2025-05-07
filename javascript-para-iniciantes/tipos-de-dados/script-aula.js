3bf864f00bce69e727c9d19e8575fdb95b4a7f0c
Carlos <cgomessilva2015@gmail.com>
2025-05-07 15:12:56 -0300

    tipos-de-dados

---
+++ javascript-para-iniciantes/tipos-de-dados/script-aula.js
@@ -0,0 +1,27 @@
+var idade = 32;
+var time = null;
+console.log(typeof idade, typeof nome, typeof time);
+
+var nome = "Carlos";
+var sobrenome = "Gomes";
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = "Romário fez " + gols + " gols";
+// console.log(typeof frase);
+
+var ano = "2018";
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = "teste";
+var frase1 = 'Esse é o "melhor" jogo';
+console.log(frase1);
+
+//Template string
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2} gols`;
+
+console.log(frase2);
---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,23 @@
+// Declare uma variável contendo uma string
+var str = "sei lá";
+// Declare uma variável contendo um número dentro de uma string
+var num = "25";
+// Declare uma variável com a sua idade
+var minhaIdade = 32;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Carlos";
+var sobrenome = "Antonio";
+var nomeCompleto = `${nome} ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(
+  typeof str,
+  typeof num,
+  minhaIdade,
+  nomeCompleto,
+  frase,
+  typeof nome
+);
