ec4db686f48280cd0729174f30d56a9ec393667a
Carlos <cgomessilva2015@gmail.com>
2025-05-07 14:26:03 -0300

    variaveis

---
+++ javascript-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,29 @@
+var nome = "Carlos";
+var idade = 32;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Carlos",
+  cidade = "jandira";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+var comida;
+console.log(comida);
+comida = "pizza";
+console.log(comida);
+
+var time = "Vasco";
+time = "Flamengo";
+
+console.log(time);
---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,19 @@
+//Exercicios
+
+// Declarar uma variável com o seu nome
+var meuNome = "Carlos";
+// Declarar uma variável com a sua idade
+var minhaIdade = 32;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Feijoada";
+// Declarar 5 variáveis diferentes sem valores
+var ano;
+var mes;
+var dia;
+var semana;
+var semestre; // Pode declarar todas na mesma linha sem repetir o var
+
+console.log(meuNome, minhaIdade, comidaFavorita);
